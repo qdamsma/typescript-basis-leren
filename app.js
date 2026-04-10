660fe347263e2dd9d5c834961078befa61860b50
Quinten <90333974+qdamsma@users.noreply.github.com>
2026-04-10 14:40:31 +0200

    Generics, types & interfaces, object en array types

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const num1Element = document.getElementById('num1');
 const num2Element = document.getElementById('num2');
 const buttonElement = document.querySelector('button');
+const numResults = [];
+const textResults = [];
 function add(num1, num2) {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         return num1 + num2;
@@ -10,12 +12,25 @@ function add(num1, num2) {
     }
     return +num1 + +num2;
 }
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
 buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1, +num2);
+    numResults.push(result);
     const stringResult = add(num1, num2);
-    console.log(result);
-    console.log(stringResult);
+    textResults.push(stringResult);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResults, textResults);
+});
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('It worked');
+    }, 1000);
+});
+myPromise.then((result) => {
+    console.log(result.split('w'));
 });
 export {};
